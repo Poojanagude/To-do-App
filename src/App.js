22746f686107196d64a1337a36c1606be68c38c8
Poojanagude <102821867+Poojanagude@users.noreply.github.com>
2022-11-21 04:21:00 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React ,{useState} from 'react';
+import Todo from './components/Todo';
+import './App.css';
+function App()
+{
+
+    return(
+    <div className="App">
+        <p></p>
+        <Todo></Todo>
+        </div>
+    )
+}
+export default App;
